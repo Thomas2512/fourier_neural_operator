8f396e74373db122e38ab7de377c5971b961bbb0
Thomas Wang <20924993+Thomas2512@users.noreply.github.com>
2021-05-14 19:33:29 +0200

    add variable #channels

--- fourier_2d_time_for_nb.py
+++ fourier_2d_time_for_nb.py
@@ -142,3 +142,83 @@ class FNO2d(nn.Module):
         gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
         gridy = gridy.reshape(1, 1, size_y, 1).repeat([batchsize, size_x, 1, 1])
         return torch.cat((gridx, gridy), dim=-1).to(device)
+
+
+class FNO2d_reac_diff(nn.Module):
+    def __init__(self, modes1, modes2, width, n_channels_in, n_channels_out):
+        super().__init__()
+
+        """
+        The overall network. It contains 4 layers of the Fourier layer.
+        1. Lift the input to the desire channel dimension by self.fc0 .
+        2. 4 layers of the integral operators u' = (W + K)(u).
+            W defined by self.w; K defined by self.conv .
+        3. Project from the channel space to the output space by self.fc1 and self.fc2 .
+        
+        input: u_t + 2 locations (u_t(x,y), x, y),  x, y)
+        input shape: (batchsize, x=64, y=64, n_channels)
+        output: the solution of the next timestep
+        output shape: (batchsize, x=64, y=64, n_channels)
+        """
+
+        self.modes1 = modes1
+        self.modes2 = modes2
+        self.width = width
+        self.n_channels_in = n_channels_in
+        self.n_channels_out = n_channels_out
+        self.fc0 = nn.Linear(self.n_channels_in, self.width)
+        # input channel is 12: the solution of the previous 10 timesteps + 2 locations (u(t-10, x, y), ..., u(t-1, x, y),  x, y)
+
+        self.conv0 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.conv1 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.conv2 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.conv3 = SpectralConv2d_fast(self.width, self.width, self.modes1, self.modes2)
+        self.w0 = nn.Conv1d(self.width, self.width, 1)
+        self.w1 = nn.Conv1d(self.width, self.width, 1)
+        self.w2 = nn.Conv1d(self.width, self.width, 1)
+        self.w3 = nn.Conv1d(self.width, self.width, 1)
+        self.bn0 = torch.nn.BatchNorm2d(self.width)
+        self.bn1 = torch.nn.BatchNorm2d(self.width)
+        self.bn2 = torch.nn.BatchNorm2d(self.width)
+        self.bn3 = torch.nn.BatchNorm2d(self.width)
+
+        self.fc1 = nn.Linear(self.width, 128)
+        self.fc2 = nn.Linear(128, self.n_channels_out)
+
+    def forward(self, x):
+        batchsize = x.shape[0]
+        size_x, size_y = x.shape[1], x.shape[2]
+
+        grid = self.get_grid(batchsize, size_x, size_y, x.device)
+        x = torch.cat((x, grid), dim=-1)
+        x = self.fc0(x)
+        x = x.permute(0, 3, 1, 2)
+
+        x1 = self.conv0(x)
+        x2 = self.w0(x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y)
+        x = self.bn0(x1 + x2)
+        x = F.relu(x)
+        x1 = self.conv1(x)
+        x2 = self.w1(x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y)
+        x = self.bn1(x1 + x2)
+        x = F.relu(x)
+        x1 = self.conv2(x)
+        x2 = self.w2(x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y)
+        x = self.bn2(x1 + x2)
+        x = F.relu(x)
+        x1 = self.conv3(x)
+        x2 = self.w3(x.view(batchsize, self.width, -1)).view(batchsize, self.width, size_x, size_y)
+        x = self.bn3(x1 + x2)
+
+        x = x.permute(0, 2, 3, 1)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        return x
+
+    def get_grid(self, batchsize, size_x, size_y, device):
+        gridx = torch.tensor(np.linspace(0, 1, size_x), dtype=torch.float)
+        gridx = gridx.reshape(1, size_x, 1, 1).repeat([batchsize, 1, size_y, 1])
+        gridy = torch.tensor(np.linspace(0, 1, size_y), dtype=torch.float)
+        gridy = gridy.reshape(1, 1, size_y, 1).repeat([batchsize, size_x, 1, 1])
+        return torch.cat((gridx, gridy), dim=-1).to(device)
